6e9f07ce8cd117543b4e27b5dcc0440edcb32aa3
Matt Garrow <mjkgarrow@gmail.com>
2024-11-15 12:19:22 +1100

    new Picture component for art direction images

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -13,3 +13,21 @@ ARTICLEWIDTH.widthCalc = `min(100% - 40px, ${ARTICLEWIDTH.maxWidth}px)`
 FIGUREWIDTH.widthCalc = `min(100% - 40px, ${FIGUREWIDTH.maxWidth}px)`
 
 export const PINWIDTH = { base: ARTICLEWIDTH.widthCalc }
+
+export const breakpoints: {
+  media: string
+  sizing: string
+}[] = [
+  { media: '(max-width: 699px)', sizing: 'mobile' },
+  { media: '(orientation: landscape) and (max-width: 979px)', sizing: 'tabletLandscape' },
+  { media: '(min-width: 700px) and (max-width: 979px)', sizing: 'tablet' },
+  { media: '(min-width: 980px) and (max-width: 1799px)', sizing: 'laptop' },
+  { media: '(min-width: 1800px)', sizing: 'desktop' }
+]
+
+export const widthsQualities = [
+  { w: 700, q: 15, dpr: 3 },
+  { w: 960, q: 30, dpr: 2 },
+  { w: 1400, q: 30, dpr: 2 },
+  { w: 2100, q: 45, dpr: 1 }
+]
